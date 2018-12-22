41d9197451c78cd541a69105d1b8f4517addf0f3
mcabrex <mrcabardo@gmail.com>
2018-12-22 01:40:00 -0500

    updated to scss, set theatric widescreen bars

---
+++ src/BlackBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const BlackBar = (props) => {
+    return (
+        <div className="bar-black">
+            what are you highlighting for?
+        </div>
+    )
+}
+
