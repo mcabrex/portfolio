480beca9f146a2f34993fef813041e9a0824524e
mcabrex <mrcabardo@gmail.com>
2019-01-05 14:58:04 -0500

    Updates

---
+++ precache-manifest.a3d1f03b61aef13a9d26e5cdf34f99a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b13b6486509b094f75c8",
+    "url": "/portfolio/static/js/main.b13b6486.chunk.js"
+  },
+  {
+    "revision": "6c11b8ae9efacecd91af",
+    "url": "/portfolio/static/js/1.6c11b8ae.chunk.js"
+  },
+  {
+    "revision": "b13b6486509b094f75c8",
+    "url": "/portfolio/static/css/main.2e9ae354.chunk.css"
+  },
+  {
+    "revision": "2620b2d9e5eb2d389efa545866bea216",
+    "url": "/portfolio/index.html"
+  }
+];
