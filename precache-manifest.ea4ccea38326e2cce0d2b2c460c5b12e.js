d16da6d6933655dd5aa35eefb30e7106c0fc10d8
mcabrex <mrcabardo@gmail.com>
2019-01-06 16:19:50 -0500

    Updates

--- precache-manifest.ea4ccea38326e2cce0d2b2c460c5b12e.js
+++ precache-manifest.ea4ccea38326e2cce0d2b2c460c5b12e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "678899e74173176df1cb",
-    "url": "/portfolio/static/js/main.678899e7.chunk.js"
+    "revision": "a8b8b1c8ecf77ce33dcc",
+    "url": "/portfolio/static/js/main.a8b8b1c8.chunk.js"
   },
   {
     "revision": "ee041ca33cead9a9a7e8",
     "url": "/portfolio/static/js/1.ee041ca3.chunk.js"
   },
   {
-    "revision": "678899e74173176df1cb",
-    "url": "/portfolio/static/css/main.622e72b0.chunk.css"
+    "revision": "a8b8b1c8ecf77ce33dcc",
+    "url": "/portfolio/static/css/main.378ab1e8.chunk.css"
   },
   {
-    "revision": "e3945e954b3f020105b3af08545081e1",
+    "revision": "8547372bb27e8c4417823972e49db6a7",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d629fb71d8cffceda528d062d82aee61.js"
+  "/portfolio/precache-manifest.ea4ccea38326e2cce0d2b2c460c5b12e.js"
 );
 
 workbox.clientsClaim();
